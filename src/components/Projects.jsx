7cae7b74c1195020e4cc53a4f9a78c062cfee545
RutviBhoraniya <rutvibhoraniya99@gmail.com>
2025-09-11 13:53:12 +0530

    live url for project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,14 +16,20 @@ const Projects = () => {
             
                 <div className='project'>
                     <img src='/image/math_logo.png' alt='brain' className='project-img'/>
-                    <a href='https://github.com/RutviBhoraniya/Mathematical-Puzzle'><h2 className='project-title'>Mathematical Puzzle</h2></a>
+                    <a href='https://mathematical-puzzle.netlify.app/home/home'><h2 className='project-title'>Mathematical Puzzle</h2></a>
                     <p>Make learning math fun by providing users with an engaging way to practice problem-solving through interactive puzzles.</p>
                 </div>
             
                 <div className='project'>
                     <img src='/image/weather.png' alt='weather' className='project-img'/>
-                    <a href='https://github.com/RutviBhoraniya/weatherApp'><h2 className='project-title'>Weather App</h2></a>
+                    <a href='https://weather-by-rrb.netlify.app/'><h2 className='project-title'>Weather App</h2></a>
                     <p>Provide users with instant, location-based weather insights through a clean and interactive interface.</p>
+                </div>
+
+                <div className='project'>
+                    <img src='/image/culture.png' alt='culture Atlas' className='project-img'/>
+                    <a href='https://cultureatlasbydr.onrender.com/'><h2 className='project-title'>Atlas</h2></a>
+                    <p>An interactive culture platform where user can share their story and culture with people over world.</p>
                 </div>
         </div>
     </div>
